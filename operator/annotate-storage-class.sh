24b8450c7e8cc9e4f554c7a5074099f67223fcd3
mmartofel <mmartofe@redhat.com>
2024-11-11 23:57:54 +0100

    annotate default storage class with k10.kasten.io/sc-supports-block-mode-exports=true

--- operator/annotate-storage-class.sh
+++ operator/annotate-storage-class.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DEFAULT_STORAGE_CLASS=$(oc get storageclass -o jsonpath='{.items[?(@.metadata.annotations.storageclass\.kubernetes\.io/is-default-class=="true")].metadata.name}')
+
 #
 # To export block PVC's to S3 we need to annotate underlying StorageClass acordingly
 #   
@@ -7,5 +9,5 @@
 #
 
 # oc annotate storageclass ocs-storagecluster-ceph-rbd \
-oc annotate storageclass ocs-external-storagecluster-ceph-rbd \
+oc annotate storageclass $DEFAULT_STORAGE_CLASS \
     k10.kasten.io/sc-supports-block-mode-exports=true
